4c2bff9244e3cafd88684ea25b86346fee60d7ff
aarekr <aare212@hotmail.com>
2022-10-18 14:31:56 +0300

    Tehtävä 2.11 step6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [hakusana, setHakusana] = useState('')
 
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
   const addPerson = (event) => {
     event.preventDefault()
     const found = persons.find(person => person.name === newName)
-    console.log("found:", found)
+    console.log('found:', found)
     if (found === undefined) {
       const nameObject = {
         name: newName,
