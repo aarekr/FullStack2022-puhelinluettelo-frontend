99e129a4fa625fc08cee8d2220f97f66cca057a0
aarekr <aare212@hotmail.com>
2022-10-18 11:24:30 +0300

    Tehtävä 2.9* step4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '1234-1234' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [hakusana, setHakusana] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -33,16 +37,30 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleHakusanaChange = (event) => {
+    console.log(event.target.value)
+    setHakusana(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input value={hakusana} onChange={handleHakusanaChange} />
+        <ul>
+          {persons
+            .filter(person => person.name.toLowerCase().includes(hakusana.toLowerCase()))
+            .map(person => <Person key={person.name} person={person} />
+          )}
+        </ul>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange} /></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div><button type="submit">add</button></div>
       </form>
-      <h2>Numbers</h2>
+      <h2>All numbers</h2>
       <ul>
         {persons.map(person => 
           <Person key={person.name} person={person} />
